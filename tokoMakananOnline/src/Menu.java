28cbbc3afda4fea56eeac50b752370f1f7ed01e8
Ida Bagus Paalakaa RNB <114908038+GusdittoRNB@users.noreply.github.com>
2023-04-11 00:04:39 +0800

    Membuat class Menu

---
+++ tokoMakananOnline/src/Menu.java
@@ -0,0 +1,17 @@
+class Menu {
+    private String name;
+    private int price;
+
+    public Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+}
--- tokoMakananOnline/src/tokoMakanan.java
+++ tokoMakananOnline/src/tokoMakanan.java
@@ -1,20 +1,5 @@
-class Menu {
-    private String name;
-    private int price;
 
-    public Menu(String name, int price) {
-        this.name = name;
-        this.price = price;
-    }
 
-    public String getName() {
-        return this.name;
-    }
-
-    public int getPrice() {
-        return this.price;
-    }
-}
 public class tokoMakanan {
     public static void main(String[] args) {
         System.out.println("Hello world!");
