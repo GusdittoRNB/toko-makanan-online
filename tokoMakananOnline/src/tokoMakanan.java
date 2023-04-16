686439946855a933157885e09bb5736077c45c43
Ida Bagus Paalakaa RNB <114908038+GusdittoRNB@users.noreply.github.com>
2023-04-16 13:20:51 +0800

    Mempercantik Tampilan dan Selesai

--- tokoMakananOnline/src/tokoMakanan.java
+++ tokoMakananOnline/src/tokoMakanan.java
@@ -43,11 +43,15 @@ public class tokoMakanan {
 
         while(pilihan != 3) {
             //tampilan menu utama
-            System.out.println("\n=== Toko Makanan Online ===");
-            System.out.println("1. Customer Login");
-            System.out.println("2. Admin Login");
-            System.out.println("3. Keluar");
-            System.out.print("Masukkan Pilihan Anda: ");
+            System.out.print("\n\t\t+-------------------------------------------+\n");
+            System.out.print("\t\t|--      PROGRAM TOKO MAKANAN ONLINE      --|\n");
+            System.out.print("\t\t+-------------------------------------------+\n");
+            System.out.print("\t\t|============[    MENU LOGIN    ]===========|\n");
+            System.out.print("\t\t|             1. Customer Login             |\n");
+            System.out.print("\t\t|             2. Admin Login                |\n");
+            System.out.print("\t\t|             3. Exit                       |\n");
+            System.out.print("\t\t+-------------------------------------------+\n");
+            System.out.print("\t\tMasukkan Pilihan Anda: ");
             pilihan = input.nextInt();
             input.nextLine(); //menggunakan satu baris baru
 
@@ -56,21 +60,27 @@ public class tokoMakanan {
                     customerLogin(input);
                     break;
                 case 2:
-                    System.out.print("Masukkan Password: ");
+                    System.out.print("\t\tMasukkan Password: ");
                     String password = input.nextLine();
+                    System.out.print("\t\t+-------------------------------------------+\n");
 
-                    if (password.equals("admin123")) {
+                    if (password.equals("Admin123")) {
                         cek = true;
                         adminLogin(input);
                     } else {
-                        System.out.println("\nPassword Salah");
+                        System.out.println("\t\tPassword Salah");
+                        System.out.print("\t\t+-------------------------------------------+\n");
                     }
                     break;
                 case 3:
-                    System.out.println("\nTerima Kasih Telah Menggunakan Program Ini");
+                    System.out.print("\t\t+-------------------------------------------+\n");
+                    System.out.println("\t\tTerima Kasih Telah Menggunakan Program Ini");
+                    System.out.print("\t\t+-------------------------------------------+\n");
                     break;
                 default:
-                    System.out.println("\nMasukkan Tidak Valid, Silahkan Ulangi");
+                    System.out.print("\t\t+-------------------------------------------+\n");
+                    System.out.println("\t\tMasukkan Tidak Valid, Silahkan Ulangi");
+                    System.out.print("\t\t+-------------------------------------------+\n");
                     break;
             }
         }
@@ -82,75 +92,94 @@ public class tokoMakanan {
 
         while(pilihan != 4) {
             //tampilkan menu dari login admin
-            System.out.println("\n=== Admin Login ===");
-            System.out.println("1. Lihat Restaurant");
-            System.out.println("2. Tambah Restaurant");
-            System.out.println("3. Hapus Restaurant");
-            System.out.println("4. Kembali");
-            System.out.print("Masukkan Pilihan Anda: ");
+            System.out.print("\n\t\t+-------------------------------------------+\n");
+            System.out.print("\t\t|--              ADMIN LOGIN              --|\n");
+            System.out.print("\t\t+-------------------------------------------+\n");
+            System.out.print("\t\t|===============[    MENU    ]==============|\n");
+            System.out.print("\t\t|             1. Lihat Restaurant           |\n");
+            System.out.print("\t\t|             2. Tambah Restaurant          |\n");
+            System.out.print("\t\t|             3. Hapus Restaurant           |\n");
+            System.out.print("\t\t|             4. Kembali                    |\n");
+            System.out.print("\t\t+-------------------------------------------+\n");
+            System.out.print("\t\tMasukkan Pilihan Anda: ");
             pilihan = input.nextInt();
+            System.out.print("\t\t+-------------------------------------------+\n");
             input.nextLine();
 
             switch(pilihan) {
                 case 1:
-                    System.out.println("\n=== Daftar Restaurant ===");
+                    System.out.println("\n\t\t===== Daftar Restaurants =====");
                     for (String restaurantName : restaurants.keySet()) {
-                        System.out.println("\n" + restaurantName + " - " + restaurants.get(restaurantName).getAddress());
-                        System.out.println("Menu: ");
+                        System.out.println("\n\t\t" + restaurantName + " - " + restaurants.get(restaurantName).getAddress());
+                        System.out.println("\t\tMenu: ");
                         for (Menu menu : restaurants.get(restaurantName).getMenus()) {
-                            System.out.println(menu.getName() + " - " + menu.getPrice());
+                            System.out.println("\t\t" + menu.getName() + " - " + menu.getPrice());
                         }
                     }
+                    System.out.print("\t\t+-------------------------------------------+\n");
                     break;
                 case 2:
-                    System.out.print("\nMasukkan Nama Restaurant: ");
+                    System.out.print("\t\tMasukkan Nama Restaurant: ");
                     String name = input.nextLine();
-                    System.out.print("Masukkan Alamat Restaurant: ");
-                    String address = input.nextLine();
+                    if(restaurants.containsKey(name)) {
+                        System.out.print("\t\t+-------------------------------------------+\n");
+                        System.out.print("\t\tNama Restaurant Tersebut Sudah Digunakan");
+                        System.out.print("\n\t\t+-------------------------------------------+\n");
+                    } else {
+                        System.out.print("\t\tMasukkan Alamat Restaurant: ");
+                        String address = input.nextLine();
 
-                    Restaurant newRestaurant = new Restaurant(name, address);
+                        Restaurant newRestaurant = new Restaurant(name, address);
 
-                    String inputMenu = "";
-                    int inputHarga = 0;
-                    String inputPilihan = "";
+                        String inputMenu = "";
+                        int inputHarga = 0;
+                        String inputPilihan = "";
 
-                    while (!inputPilihan.equals("n")) {
-                        System.out.print("Masukkan Nama Menu: ");
-                        inputMenu = input.nextLine();
-                        System.out.print("Masukkan Harga Menu: ");
-                        inputHarga = input.nextInt();
-                        input.nextLine();
+                        while (!inputPilihan.equals("n")) {
+                            System.out.print("\t\t+-------------------------------------------+\n");
+                            System.out.print("\t\tMasukkan Nama Menu: ");
+                            inputMenu = input.nextLine();
+                            System.out.print("\t\tMasukkan Harga Menu: ");
+                            inputHarga = input.nextInt();
+                            input.nextLine();
 
-                        newRestaurant.addMenu(new Menu(inputMenu, inputHarga));
+                            newRestaurant.addMenu(new Menu(inputMenu, inputHarga));
 
-                        System.out.print("Apakah Ingin Menambah Menu Lagi? (y/n): ");
-                        inputPilihan = input.nextLine();
+                            System.out.print("\t\tApakah Ingin Menambah Menu Lagi? (y/n): ");
+                            inputPilihan = input.nextLine();
+                        }
+                        restaurants.put(name, newRestaurant);
+                        System.out.print("\t\t+-------------------------------------------+\n");
+                        System.out.println("\t\tRestaurant Berhasil Ditambahkan");
+                        System.out.print("\t\t+-------------------------------------------+\n");
                     }
-                    restaurants.put(name, newRestaurant);
-                    System.out.println("\nRestaurant Berhasil Ditambahkan");
                     break;
                 case 3:
                     String inputTanya = "";
-                    System.out.print("\nMasukkan Nama Restaurant: ");
+                    System.out.print("\t\tMasukkan Nama Restaurant: ");
                     String restaurantName = input.nextLine();
                     if(restaurants.containsKey(restaurantName)) {
-                        System.out.print("Apakah Yakin Untuk Menghapusnya? (y/n): ");
+                        System.out.print("\t\tApakah Yakin Untuk Menghapusnya? (y/n): ");
                         inputTanya = input.nextLine();
+                        System.out.print("\t\t+-------------------------------------------+\n");
                         if(inputTanya.equals("y")) {
                             restaurants.remove(restaurantName);
-                            System.out.println("\nRestaurant Berhasil Dihapus");
+                            System.out.println("\t\tRestaurant Berhasil Dihapus");
                         }else {
-                            System.out.println("\nRestaurant Tidak Berhasil Dihapus");
+                            System.out.println("\t\tRestaurant Tidak Berhasil Dihapus");
                         }
                     }else {
-                            System.out.println("\nRestaurant Tidak Ditemukan");
+                        System.out.print("\t\t+-------------------------------------------+\n");
+                        System.out.println("\t\tRestaurant Tidak Ditemukan");
                         }
+                    System.out.print("\t\t+-------------------------------------------+\n");
                     break;
                 case 4:
                     boolean cek = false;
                     break;
                 default:
-                    System.out.println("\nMasukkan Tidak Valid, Silahkan Ulangi");
+                    System.out.println("\t\tMasukkan Tidak Valid, Silahkan Ulangi");
+                    System.out.print("\t\t+-------------------------------------------+\n");
                     break;
             }
         }
@@ -160,31 +189,39 @@ public class tokoMakanan {
         int pilihan = 0;
         ArrayList<Order> orders = new ArrayList<>();
 
-        System.out.print("Masukkan Nama Anda: ");
+        System.out.print("\t\tMasukkan Nama Anda: ");
         String namaPelanggan = input.nextLine();
-        System.out.print("\nSelamat Datang " + namaPelanggan);
-        System.out.println();
+        System.out.print("\t\t+-------------------------------------------+\n");
+        System.out.print("\n\t\t|=======[   SELAMAT DATANG " + namaPelanggan + "   ]=======|\n");
 
         while(pilihan != 3) {
             //tampilkan menu dari login customer
-            System.out.println("\n=== Customer Login ===");
-            System.out.println("1. Pesan Makanan Atau Minuman");
-            System.out.println("2. Lihat Pesanan");
-            System.out.println("3. Kembali");
-            System.out.print("Masukkan Pilihan Anda: ");
+            System.out.print("\n\t\t+-------------------------------------------+\n");
+            System.out.print("\t\t|--             CUSTOMER LOGIN            --|\n");
+            System.out.print("\t\t+-------------------------------------------+\n");
+            System.out.print("\t\t|===============[    MENU    ]==============|\n");
+            System.out.print("\t\t|       1. Pesan Makanan Atau Minuman       |\n");
+            System.out.print("\t\t|       2. Lihat Pesanan                    |\n");
+            System.out.print("\t\t|       3. Kembali                          |\n");
+            System.out.print("\t\t+-------------------------------------------+\n");
+            System.out.print("\t\tMasukkan Pilihan Anda: ");
             pilihan = input.nextInt();
+            System.out.print("\t\t+-------------------------------------------+\n");
             input.nextLine();
 
             switch(pilihan) {
                 case 1:
-                    System.out.println("\n==== Restaurants ====");
+                    System.out.print("\n\t\t|============[   RESTAURANTS   ]============|\n");
                     for (String restaurantName : restaurants.keySet()) {
-                        System.out.println(restaurantName + " - " + restaurants.get(restaurantName).getAddress());
+                        System.out.println("\t\t" + restaurantName + " - " + restaurants.get(restaurantName).getAddress());
                     }
-                    System.out.print("\nMasukkan Nama Restaurant: ");
+                    System.out.print("\t\t+-------------------------------------------+\n");
+                    System.out.print("\t\tMasukkan Nama Restaurant: ");
                     String restaurantName = input.nextLine();
+                    System.out.print("\t\t+-------------------------------------------+\n");
                     if (!restaurants.containsKey(restaurantName)) {
-                        System.out.println("\nRestaurant Tersebut Tidak Terdaftar");
+                        System.out.println("\t\tRestaurant Tersebut Tidak Terdaftar");
+                        System.out.print("\t\t+-------------------------------------------+\n");
                         break;
                     }
 
@@ -193,49 +230,53 @@ public class tokoMakanan {
 
                     String inputTambah = "";
                     while (!inputTambah.equals("n")) {
-                        System.out.println("\n==== " + restaurant.getName() + " ====");
-                        System.out.println("Menu:");
+                        System.out.println("\n\t\t===== " + restaurant.getName() + " =====");
+                        System.out.println("\t\tMenu:");
                         int nomorMenu = 1;
                         for (Menu menu : restaurant.getMenus()) {
-                            System.out.println(nomorMenu + ". " + menu.getName() + " - " + menu.getPrice());
+                            System.out.println("\t\t" + nomorMenu + ". " + menu.getName() + " - " + menu.getPrice());
                             nomorMenu++;
                         }
-                        System.out.print("\nMasukkan Nomor Menu Yang Akan Dipesan: ");
+                        System.out.print("\t\t+-------------------------------------------+\n");
+                        System.out.print("\t\tMasukkan Nomor Menu Yang Akan Dipesan: ");
                         int indexMenu = input.nextInt();
-                        System.out.print("Masukkan Jumlahnya: ");
+                        System.out.print("\t\tMasukkan Jumlahnya: ");
                         int jumlah = input.nextInt();
                         input.nextLine();
 
                         newOrder.addMenu(restaurant.getMenus().get(indexMenu - 1), jumlah);
 
-                        System.out.print("Apakah Ingin Menambah Pesanan Lagi? (y/n): ");
+                        System.out.print("\t\tApakah Ingin Menambah Pesanan Lagi? (y/n): ");
                         inputTambah = input.nextLine();
                     }
 
-                    System.out.print("Masukkan Jarak Anda Dengan Restaurant (dalam km): ");
+                    System.out.print("\t\tMasukkan Jarak Dengan Restaurant (dalam km): ");
                     int jarak = input.nextInt();
 
                     newOrder.setDistance(jarak);
                     orders.add(newOrder);
-                    System.out.println("\nPesanan " + namaPelanggan + " Berhasil Diorder");
+                    System.out.print("\t\t+-------------------------------------------+\n");
+                    System.out.println("\t\tPesanan " + namaPelanggan + " Berhasil Diorder");
+                    System.out.print("\t\t+-------------------------------------------+\n");
                     break;
                 case 2:
-                    System.out.println("\n==== Orderan " + namaPelanggan + " ====");
+                    System.out.println("\n\t\t======= Orderan " + namaPelanggan + " =======");
                     for (int i = 0; i < orders.size(); i++) {
                         Order order = orders.get(i);
-                        System.out.println("\n" + (i + 1) + ". " + order.getRestaurant().getName());
-                        System.out.println("Menu:");
+                        System.out.println("\n\t\t" + (i + 1) + ". " + order.getRestaurant().getName());
+                        System.out.println("\t\tMenu:");
                         for (Menu menu : order.getMenus().keySet()) {
-                            System.out.println(menu.getName() + " x " + order.getMenus().get(menu));
+                            System.out.println("\t\t" + menu.getName() + " x " + order.getMenus().get(menu));
                         }
-                        System.out.println("Jarak (ongkir: 5000/km): " + order.getDistance() + " km");
-                        System.out.println("Total Harga: " + order.getTotalPrice());
+                        System.out.println("\t\tJarak (ongkir: 5000/km): " + order.getDistance() + " km");
+                        System.out.println("\t\tTotal Harga: " + order.getTotalPrice());
                     }
                     break;
                 case 3:
                     break;
                 default:
-                    System.out.println("\nMasukkan Tidak Valid, Silahkan Ulangi");
+                    System.out.println("\t\tMasukkan Tidak Valid, Silahkan Ulangi");
+                    System.out.print("\t\t+-------------------------------------------+\n");
             }
         }
     }
